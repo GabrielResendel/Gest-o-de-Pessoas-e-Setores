78b4c224e4eae38be42fc78d2a499b058ac29bf2
unknown <codegabriel7@gmail.com>
2025-10-15 19:10:53 -0300

    Git inicial ASP NET MVC

---
+++ Gestão de pessoas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gestão_de_pessoas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
